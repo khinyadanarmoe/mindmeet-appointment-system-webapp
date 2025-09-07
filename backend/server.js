c023e5044e11e6f31b145707b59de758f65bfa44
khinyadanarmoe <khinyadanamoe01@gmail.com>
2025-09-08 00:36:25 +0700

    mongodb set up

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,22 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import authRoutes from "./routes/auth.js";
-import doctorRoutes from "./routes/doctor.js";
-import appointmentRoutes from "./routes/appointment.js";
-import userRoutes from "./routes/user.js";
+import  "dotenv/config";
+import connectDB from "./config/mongodb.js";
 
-// Load environment variables from .env file
-dotenv.config();
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
+connectDB();
 
 // Middleware
+
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// api endpoints
+app.get('/',(req, res) => {
+    res.send('API is working')
+})
 
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/doctors", doctorRoutes);
-app.use("/api/appointments", appointmentRoutes);
-app.use("/api/users", userRoutes);
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-}); 
+app.listen(PORT, () => console.log('server started at', PORT))
