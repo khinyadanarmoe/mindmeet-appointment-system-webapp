22c00d44c4b21afb00bdf3a1ff2012cba00747b6
khinyadanarmoe <khinyadanamoe01@gmail.com>
2025-09-20 02:21:02 +0700

    minor fix

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '/mindmeet/admin' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/mindmeet/' : '',
   images: {
     domains: ['res.cloudinary.com'],
   },
