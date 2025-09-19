11984ef1940a268615e22a3abf1c9679d0def3a8
khinyadanarmoe <khinyadanamoe01@gmail.com>
2025-09-20 00:18:48 +0700

    add base path

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.NODE_ENV === 'production' ? '/mindmeet' : '',
   images: {
     domains: ['res.cloudinary.com'],
+    unoptimized: process.env.NODE_ENV === 'production',
   },
 };
 
